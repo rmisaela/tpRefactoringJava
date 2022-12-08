33a0d9024a34ecb6207dd42df49e6290d368f067
Misaela <randrianasolo.m@zurcher.edu.mg>
2022-12-08 19:47:43 +0300

    Add package and Class with attr and Constructor

---
+++ tpRefactoring/src/iut/bad/Homme.java
@@ -0,0 +1,39 @@
+package iut.bad;
+
+public class Homme {
+	
+	protected String nom;
+	protected String prenom;
+	protected int age;
+	
+	public Homme(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	
+	public Homme(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = 0;
+	}
+
+	public Homme(String nom) {
+		super();
+		this.nom = nom;
+		this.prenom = "";
+		this.age = 0;
+	}
+
+
+	public Homme() {
+		super();
+		this.nom = "Adam";
+		this.prenom = "";
+		this.age = 3000;
+	}
+	
+	
+}
